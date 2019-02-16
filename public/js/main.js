b61de7fe29bafbd0287a93ce0d19a9b86e8af456
Zack Holmberg <zack_holmberg@outlook.com>
2019-02-15 18:26:14 -0600

    Added navbar and sidebar.
    Added page navigation through top navbar and sidebar one mobile. Also removed some unised code from css and js libraries.

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+'use strict'; 
+
+
+$(document).ready( function() {	
+    
+    
+    // HOME TYPED JS
+    if ($('.element').length) {
+        $('.element').each(function () {
+            $(this).typed({
+                strings: [$(this).data('text1'), $(this).data('text2'), $(this).data('text3')],
+                loop: $(this).data('loop') ? $(this).data('loop') : false ,
+                backDelay: $(this).data('backdelay') ? $(this).data('backdelay') : 2000 ,                
+                typeSpeed: 10,
+            });
+        });
+    }
+
+    $(document).ready(function() {   
+        var sideslider = $('[data-toggle=collapse-side]');
+        var sel = sideslider.attr('data-target');
+        var sel2 = sideslider.attr('data-target-2');
+        sideslider.click(function(event){
+            $(sel).toggleClass('in');
+            $(sel2).toggleClass('out');
+        });
+    });
+    
+    
+}); // document ready end 
+
+
